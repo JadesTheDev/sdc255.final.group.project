fd5ab5c2d377e65196e71419c7ca6efffa57e36e
leapit6717 <leapit6717@students.ecpi.edu>
2026-01-21 15:26:50 -0500

    Update calculations.h

--- calculations.h
+++ calculations.h
@@ -1,1 +1,21 @@
+// calculations.h
+//
+// Header file for calculation functions
+// (User Story #X - Replace with your actual user story)
+//
+// Requirements:
+// - Exposes calculation functions to be called externally
+// - Includes header guards to prevent multiple inclusion
+//
+
+#ifndef CALCULATIONS_H
+#define CALCULATIONS_H
+
+// Function prototypes
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+double divide(int a, int b);
+
+#endif // CALCULATIONS_H
 
