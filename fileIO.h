56395c6f32941aebfa9b52cb9df9c9e84018be14
noawil1379 <noawil1379@students.ecpi.edu>
2026-01-23 11:24:38 -0500

    Update fileIO.h

--- fileIO.h
+++ fileIO.h
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+void writetoFile(const char *fileIO.h) {
+  FILE *file = fopen(fileIO.h, "w");
+
+  if (file == NULL) {
+    printf("Error opening file.\n");
+    return;
+  }
+  fputs("FileIO\n", file);
+
+  fclose(file);
+}
