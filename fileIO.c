9a56df09ac4eda9d681c03df10aae2225a63869b
noawil1379 <noawil1379@students.ecpi.edu>
2026-01-23 11:13:46 -0500

    Update fileIO.c

--- fileIO.c
+++ fileIO.c
@@ -1,1 +1,30 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+ 
+char readFile(const char *fileIO.c) {
+FILE *file = fopen(fileIO.c, "r");
+char *buffer;
+int ch;
+int size = 0;
+ 
+if (file == NULL) {
+return NULL;
+}
+ 
+buffer = malloc(1);
+if (buffer == NULL) {
+fclose(file);
+return NULL;
+}
+ 
+while ((ch = fgetc(file)) != EOF (
+buffer = realloc(buffer, size + 2);
+buffer[size] = ch;
+size++;
+}
+ 
+buffer[size] = '\0';
+ 
+fclose(file);
+return buffer;
