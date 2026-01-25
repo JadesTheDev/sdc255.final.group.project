b5b356d8c8b90c48ed960a37650717ccd32a4393
JadesTheDev <jades.the.dev@gmail.com>
2026-01-24 21:07:21 -0500

    Update fileIO.c

--- noah/fileIO.c
+++ noah/fileIO.c
@@ -1,33 +1,35 @@
-//fileIO.c
-//Implement readFile()
-
+// fileIO.c
 #include <stdio.h>
 #include <stdlib.h>
 #include "fileIO.h"
- 
-char readFile(const char *fileIO.c) {
-FILE *file = fopen(fileIO.c, "r");
-char *buffer;
-int ch;
-int size = 0;
- 
-if (file == NULL) {
-return NULL;
+
+char* readFile(const char *filename)
+{
+    FILE *file = fopen(filename, "r");
+    char *buffer;
+    int ch;
+    int size = 0;
+
+    if (!file) return NULL;
+
+    buffer = malloc(1);
+    if (!buffer) {
+        fclose(file);
+        return NULL;
+    }
+
+    while ((ch = fgetc(file)) != EOF) {
+        char *temp = realloc(buffer, size + 2);
+        if (!temp) {
+            free(buffer);
+            fclose(file);
+            return NULL;
+        }
+        buffer = temp;
+        buffer[size++] = ch;
+    }
+
+    buffer[size] = '\0';
+    fclose(file);
+    return buffer;
 }
- 
-buffer = malloc(1);
-if (buffer == NULL) {
-fclose(file);
-return NULL;
-}
- 
-while ((ch = fgetc(file)) != EOF (
-buffer = realloc(buffer, size + 2);
-buffer[size] = ch;
-size++;
-}
- 
-buffer[size] = '\0';
- 
-fclose(file);
-return buffer;
