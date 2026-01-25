e295a8e83dba0a14f15a96e5af464658c8463913
JadesTheDev <jades.the.dev@gmail.com>
2026-01-24 21:13:52 -0500

    Update fileIO.c

--- noah/fileIO.c
+++ noah/fileIO.c
@@ -1,4 +1,6 @@
 // fileIO.c
+// Implements readFile() and writeFile()
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "fileIO.h"
@@ -10,26 +12,44 @@ char* readFile(const char *filename)
     int ch;
     int size = 0;
 
-    if (!file) return NULL;
+    if (file == NULL) return NULL;
 
-    buffer = malloc(1);
-    if (!buffer) {
+    buffer = (char*)malloc(1);
+    if (buffer == NULL)
+    {
         fclose(file);
         return NULL;
     }
 
-    while ((ch = fgetc(file)) != EOF) {
-        char *temp = realloc(buffer, size + 2);
-        if (!temp) {
+    while ((ch = fgetc(file)) != EOF)
+    {
+        char *temp = (char*)realloc(buffer, size + 2);
+        if (temp == NULL)
+        {
             free(buffer);
             fclose(file);
             return NULL;
         }
         buffer = temp;
-        buffer[size++] = ch;
+        buffer[size++] = (char)ch;
     }
 
     buffer[size] = '\0';
     fclose(file);
     return buffer;
+}
+
+void writeFile(const char *filename)
+{
+    FILE *file = fopen(filename, "w");
+    if (file == NULL)
+    {
+        printf("Error opening file for writing.\n");
+        return;
+    }
+
+    fputs("FileIO\n", file);
+    fputs("This file was written by writeFile().\n", file);
+
+    fclose(file);
 }
